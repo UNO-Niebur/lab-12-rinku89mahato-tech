edcce68ddbef9c1e5dec996135478e8a485612e6
rinku89mahato-tech <rinku89mahato@gmail.com>
2026-04-26 13:52:51 +0000

    Completed_SearchSortLab.py

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 # SearchSortLab.py
-# Name:
-# Date:
+# Name:Rinku Mahato
+# Date:04/26/2026
 # Assignment: Lab 13 – Searching and Sorting
-
+#Part 4 – Reflection Questions
+#Which list was easiest to sort? Why? Sorted list was the easiest to sort due to it needs very few swaps.
+# Which list required the most work? Reverse list required the most work because random list is still partially correct in position.
+# Why is sorting useful before searching? Sorting is useful for binary search.
+# When might linear search still be useful? linear search is useful for unsorted data.
 
 def linearSearch(data, target):
     """Return the index of target if found, otherwise return -1."""
     
     # TODO: implement linear search
+    for i in range(len(data)):
+        if data[i] == target:
+            return i
     
     return -1
 
@@ -16,6 +23,13 @@ def bubbleSort(data):
     """Sort the list using bubble sort and return the sorted list."""
     
     # TODO: implement bubble sort
+    n = len(data)
+
+    for pass_num in range(n-1):
+        for i in range(n - 1 - pass_num):
+            if data[i] > data [i + 1]:
+                data[i], data[i + 1] = data [i +1], data[i]
+
     
     return data
 
